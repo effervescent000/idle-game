e7a8eee0d145c3b2313c7c64cd376b7309bd21ff
effervescent000 <kislio01@gmail.com>
2023-07-06 09:03:48 -0400

    create button

---
+++ src/components/common/button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	// PROPS
+	export let classes = '';
+	export let type: 'submit' | 'button' = 'button';
+
+	// STATE
+
+	// LOGIC
+</script>
+
+<button {type} class={`bg-accent-secondary border-accent-primary border rounded-md p-1 ${classes}`}>
+	<slot />
+</button>
---
+++ src/routes/recruit/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	// PROPS
+
+	import Button from '../../components/common/button.svelte';
+
+	// STATE
+
+	// LOGIC
+</script>
+
+<Button>Find recruits</Button>
