4b84603b2467e443d73ecbd4bd17fd0aa358e8ce
effervescent000 <kislio01@gmail.com>
2023-07-07 09:34:18 -0400

    render recruits

--- src/routes/recruit/+page.server.ts
+++ src/routes/recruit/+page.server.ts
@@ -2,10 +2,15 @@ import { redirect } from '@sveltejs/kit';
 import { makeGetRequest } from '../../utils/backend-api-service.js';
 import { recruitStore } from '../../stores/roster-stores.js';
 
+export const load = async () => {
+	const result = await makeGetRequest('/recruit');
+	return { data: result };
+};
+
 export const actions = {
 	fetch: async () => {
 		const result = await makeGetRequest('/recruit');
-		recruitStore.set(result);
-		throw redirect(303, '/recruit');
+		// recruitStore.set(result);
+		return { data: result };
 	}
 };
--- src/routes/recruit/+page.svelte
+++ src/routes/recruit/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 
+	import type { Recruit } from '../../types/heroes';
+
 	import Button from '../../components/common/button.svelte';
+	import HeroCard from '../../components/heroes/hero-card.svelte';
 
 	// PROPS
+	export let data: { data: Recruit[] };
 
 	// STATE
 
@@ -11,6 +15,13 @@
 </script>
 
 <form method="post" action="?/fetch" use:enhance>
-	<!-- TODO make this a submit button when we're ready -->
 	<Button type="submit">Find recruits</Button>
 </form>
+
+<div>
+	{#if data?.data}
+		{#each data.data as recruit}
+			<HeroCard hero={recruit} />
+		{/each}
+	{/if}
+</div>
